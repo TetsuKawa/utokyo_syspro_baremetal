4ccc59bcd1798965ffaf714930a401e28070b409
TetsuKawa <kawaguchitnon@icloud.com>
2025-01-29 00:10:53 +0900

    feat: test kadai1B1
    Signed-off-by: TetsuKawa <kawaguchitnon@icloud.com>

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,6 +1,7 @@
 #include "hardware.h"
 #include "segmentation.h"
 #include "util.h"
+#include "pm_timer.h"
 
 void start(void *SystemTable __attribute__ ((unused)), struct HardwareInfo *_hardware_info) {
   // From here - Put this part at the top of start() function
@@ -11,7 +12,15 @@ void start(void *SystemTable __attribute__ ((unused)), struct HardwareInfo *_har
 
   // To here - sample code
   init_frame_buffer(&hardware_info.fb);
-
+  init_acpi_pm_timer(hardware_info.rsdp);
+  puts("Timer Start!\r\n");
+  int i = 0;
+  while (i < 50) {
+    pm_timer_wait_millisec(1000);
+    puts("1000ms passed!\r\n");
+    i++;
+  }
+  puts("Timer End!\r\n");
   // Do not delete it!
   while (1);
 }
